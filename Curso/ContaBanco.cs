99bee387bcd182d045194549ca5becec77ec5837
vlvsjamalzz <vlvs.jamalzz@gmail.com>
2023-10-11 15:38:35 -0300

    first commit

---
+++ Curso/ContaBanco.cs
@@ -0,0 +1,119 @@
+﻿using System.Globalization;
+namespace Course
+{
+    class ContaBanco
+    {
+        public int numConta;
+        protected string tipo;
+        private string dono;
+        private double saldo;
+        private bool status;
+        
+        public string abrirConta(string Abrir)
+        {
+            if (Abrir == "Sim" && status != true)
+            {
+                setnumConta();
+                setstatus(true);                
+                Console.WriteLine("Qual tipo de conta? Conta Corrente(CC) ou Conta Poupança(CP)");
+                setTipo(Console.ReadLine()); 
+                if (tipo == "ContaCorrente")
+                {
+                    setSaldo(50.0);
+                }
+                else if (tipo == "ContaPoupança")
+                {
+                    setSaldo(150.0);
+                }
+            }
+            
+            return Abrir;
+        }
+
+       /* public string fecharConta()
+        {
+
+        }
+        public double depositar()
+        {
+
+        }*/
+
+        public double sacar()
+        {
+            Console.WriteLine("Quanto deseja sacar?");
+            double.Parse(Console.ReadLine());
+            saldo = saldo - sacar();
+            return saldo;
+        }
+
+        public double pagarMensal()
+        {
+            if(tipo == "ContaCorrente")
+            {
+                saldo = saldo - 12.00;
+            }
+            else if(tipo == "ContaPoupança")
+            {
+                saldo = saldo - 20.00;
+            }
+            return saldo;
+        }
+
+        public int getnumConta()
+        {
+            return numConta;
+        }
+
+        public void setnumConta()
+        {
+            numConta = numConta + 1;
+        }
+
+        public string getTipo()
+        {
+            return tipo;
+        }
+        
+        public void setTipo(string Tipo)
+        {
+            if (Tipo == "CC")
+            {
+                tipo = "ContaCorrente";               
+            }
+            else if (Tipo == "CP")
+            {
+                tipo = "ContaPoupança";                
+            }
+        }
+
+        public string getDono()
+        {
+            return dono;
+        }
+
+        public void setDono() 
+        { 
+
+        }
+
+        public double getSaldo()
+        {
+            return saldo;
+        }
+
+        public void setSaldo(double Saldo)
+        {
+            saldo = Saldo;
+        }
+
+        public bool getStatus()
+        {
+            return status;
+        }
+        public void setstatus(bool Status)
+        {
+            status = Status;
+        }
+    }
+}
---
+++ Curso/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBanco p = new ContaBanco();
+
+            Console.WriteLine("Deseja Abrir uma conta bancária?");
+            p.abrirConta(Console.ReadLine());
+            Console.WriteLine(p.getnumConta());
+            Console.WriteLine(p.getTipo());
+            Console.WriteLine(p.getStatus());
+            Console.WriteLine(p.getSaldo().ToString("f2"));
+            p.pagarMensal();
+            Console.WriteLine(p.getSaldo().ToString("f2"));
+            p.sacar();
+            Console.WriteLine(p.getSaldo().ToString("f2"));
+        }
+    }
+}
