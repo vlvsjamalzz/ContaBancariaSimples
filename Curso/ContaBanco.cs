983ccaad16d90707935c6bdfeeebed12e1e00c17
VICTOR LIMA <VICTOR LIMA@DESKTOP-PCRS60E>
2023-10-15 23:55:29 -0300

    Término das implementações de todos os métodos solicitados.

--- Curso/ContaBanco.cs
+++ Curso/ContaBanco.cs
@@ -13,8 +13,9 @@ namespace Course
         {
             if (Abrir == "Sim" && status != true)
             {
-                setnumConta();
                 setstatus(true);
+                setDono();
+                setnumConta();
                 Console.WriteLine("Qual tipo de conta? Conta Corrente(CC) ou Conta Poupança(CP)");
                 setTipo(Console.ReadLine());
                 if (tipo == "ContaCorrente")
@@ -25,25 +26,61 @@ namespace Course
                 {
                     setSaldo(150.0);
                 }
-            }            
+            }
+            else
+            {
+                Console.WriteLine("Encerrando abertura de conta.");
+            }
             
         }
 
-       /* public string fecharConta()
+        public void fecharConta()
         {
-
+            Console.WriteLine("Deseja mesmo encerrar sua conta?");
+            string encerrar = Console.ReadLine();
+            if (encerrar == "Sim" && status == true && saldo == 0.0)
+            {
+                setstatus(false);
+            }
+            else if (encerrar == "Sim" && status != true)
+            {
+                Console.WriteLine("Essa conta não existe.");
+            }
+            else
+            {
+                Console.WriteLine("Sua conta possui pendências. Entre em contato para solucionar antes do encerramento da conta.");
+            }
         }
 
-        public double depositar()
+        public void depositar()
         {
-
-        }*/
+            Console.WriteLine("Deseja fazer um depósito? (SIM ou NÃO)");
+            string Depositar = Console.ReadLine();
+            if (Depositar == "Sim" && status == true)
+            {
+                Console.WriteLine("Digite o valor do depósito: ");
+                double deposito = double.Parse(Console.ReadLine());
+                saldo = saldo + deposito;
+            }
+            else if (Depositar == "Sim" && status != true)
+            {
+                Console.WriteLine("Conta inexistente.");
+            }
+            else if (Depositar == "Não" && status == true)
+            {
+                Console.WriteLine("Encerrando painel de depósito");
+            }
+            else
+            {
+                Console.WriteLine("Não entendi, por favor digite SIM se deseja fazer um depósito ou NÃO para sair.");
+            }
+        }
 
         public void sacar()
         {
-            Console.WriteLine("Deseja fazer um saque?");
+            Console.WriteLine("Deseja fazer um saque? (SIM ou NÃO)");
             string Sacar = Console.ReadLine();
-            if (Sacar == "Sim" && status != false) 
+            if (Sacar == "Sim" && status == true) 
             {
                 Console.WriteLine("Digite o valor do saque: ");
                 double saque = double.Parse(Console.ReadLine());
@@ -54,25 +91,37 @@ namespace Course
                 else
                 {
                     Console.WriteLine("Saldo insuficiente.");
-                    Console.WriteLine("Seu saldo atual é: ", getSaldo().ToString("F2"));
+                    Console.WriteLine("Seu saldo atual é: R$", getSaldo().ToString("F2"));
                 }                
-            } 
-            else
+            }
+            else if (Sacar == "Sim" && status != true)
             {
                 Console.WriteLine("Conta inexistente.");
+            }
+            else if (Sacar == "Não" && status == true)
+            {
+                Console.WriteLine("Encerrando painel de saque");
+            }
+            else
+            {
+                Console.WriteLine("Não entendi, por favor digite SIM se deseja fazer um saque.");
             }
         }
 
         public void pagarMensal()
         {
-            if(tipo == "ContaCorrente")
+            if(tipo == "ContaCorrente" && status == true)
             {
                 saldo = saldo - 12.00;
             }
-            else if(tipo == "ContaPoupança")
+            else if(tipo == "ContaPoupança" && status == true)
             {
                 saldo = saldo - 20.00;
-            }            
+            }
+            else
+            {
+                Console.WriteLine("Conta inexistente.");
+            }
         }
 
         public int getnumConta()
@@ -86,7 +135,7 @@ namespace Course
         }
 
         public string getTipo()
-        {
+        {            
             return tipo;
         }
         
@@ -108,8 +157,11 @@ namespace Course
         }
 
         public void setDono() 
-        { 
-
+        {
+            Console.WriteLine("Digite o nome do titular da conta: ");
+            string Titular = Console.ReadLine();
+            dono = Titular;
+            
         }
 
         public double getSaldo()
--- Curso/Program.cs
+++ Curso/Program.cs
@@ -8,16 +8,60 @@ namespace Course
         {
             ContaBanco p = new ContaBanco();
 
-            Console.WriteLine("Deseja Abrir uma conta bancária?");
-            p.abrirConta(Console.ReadLine());
-            Console.WriteLine(p.getnumConta());
-            Console.WriteLine(p.getTipo());
-            Console.WriteLine(p.getStatus());
-            Console.WriteLine(p.getSaldo().ToString("f2"));
-            p.pagarMensal();
-            Console.WriteLine(p.getSaldo().ToString("f2"));
-            p.sacar();
-            Console.WriteLine(p.getSaldo().ToString("f2"));
+            int opcoes = 0;
+
+            do
+            {
+                Console.WriteLine("Escolha a ação que deseja realizar: ");
+                Console.WriteLine("0 - Abrir conta");
+                Console.WriteLine("1 - Depositar");
+                Console.WriteLine("2 - Sacar");
+                Console.WriteLine("3 - Verificar Saldo bancário atual");
+                Console.WriteLine("4 - Verificar o número de sua conta");
+                Console.WriteLine("5 - Verificar o o tipo de sua conta");
+                Console.WriteLine("6 - Verificar o nome do titular da conta");
+                Console.WriteLine("7 - Verificar o status atual da conta");
+                Console.WriteLine("8 - Fechar a sua conta");
+                Console.WriteLine("9 - Sair do Internet Banking");
+
+                opcoes = int.Parse(Console.ReadLine());
+
+                switch (opcoes)
+                {
+                    case 0:
+                        p.abrirConta(Console.ReadLine());
+                        break;
+                    case 1:
+                        p.depositar();
+                        break;
+                    case 2:
+                        p.sacar();
+                        break;
+                    case 3:
+                        Console.WriteLine(p.getSaldo());
+                        break;
+                    case 4:
+                        Console.WriteLine(p.getnumConta());
+                        break;
+                    case 5:
+                        Console.WriteLine(p.getTipo());
+                        break;
+                    case 6:
+                        Console.WriteLine(p.getDono());
+                        break;
+                    case 7:
+                        Console.WriteLine(p.getStatus());
+                        break;
+                    case 8:
+                        break;
+                    case 9:
+                        Console.WriteLine("Obrigado por ser cliente.");
+                        break;
+                }
+            }
+            while (opcoes != 9);
         }
     }
 }
+
+
